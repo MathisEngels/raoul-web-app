647cf4a77580f0aec6e92cc47e409afd22e060cf
mathis <m2505e99>
2021-07-27 14:23:41 +0200

    fix request port

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function App() {
       setError(false);
       const toastId = toast.loading("Envoi...");
       const ip = await publicIp.v4();
-      axios.post("http://localhost:3000/message", { email, message, ip })
+      axios.post("http://localhost:8000/message", { email, message, ip })
         .then(function (response) {
           if (response.status === 200) {
             toast.success("Message envoyé !", {
